53b72b94edced84e67d9e9f894c837c9aab17f19
ZDQ8230 <zdq8230@gmail.com>
2022-11-17 19:38:45 +1100

    more update (table and chart on streamlit

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ st.title("Crypto Portfolio Dashboard and Personal Tax Calculation")
 
 # Ask user to input their salary 
 
-st.number_input("Input Salary")
+income = st.number_input("Input Salary")
 
 #Ask user to upload transcation file 
 
@@ -17,8 +17,24 @@ file_uploader = st.file_uploader('Upload crypto transaction file (.csv)')
 
 # Transform the upload file into dataframe 
 
-df = pd.read_csv(file_uploader, infer_datetime_format= True)
+df = pd.read_csv(file_uploader)
 
 # Displaying the user input CSV
 
-st.table(df) 
+st.write(df)
+
+#For chart. 
+
+#For Table of tax income content:
+
+table = pd.DataFrame()
+
+table["Financial year"] = pd.groupby(time_index_year)
+
+table["Salary Income"] = income
+
+table["Capital Gain/Loss from Crypto Investment"] = 
+
+table["Capital Gain Income Tax (null if Capital Loss"] = 
+
+st.table(table)
